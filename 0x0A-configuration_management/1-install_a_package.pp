d7e6767d4a403d89b1f4f4302276bbaee46257b5
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2024-04-19 14:40:26 +0100

    Installing flask and Werkzeug from pip3 using puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,8 @@
-# Using Puppet, install flask from pip3
+# Installing flask and Werkzeug from pip3 using puppet
+package { 'Werkzeug':
+    ensure   => '2.1.1',
+    provider => 'pip3',
+}
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
