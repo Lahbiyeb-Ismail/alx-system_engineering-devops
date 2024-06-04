b0e372936ca35731012327ab9d13f3b6d835696b
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-06-04 16:41:09 +0100

    test

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,8 +8,6 @@ subreddit. If an invalid subreddit is given, the function should return 0.
 
 import requests
 
-BASE_URL = "https://www.reddit.com/r"
-
 
 def number_of_subscribers(subreddit):
     """
@@ -22,13 +20,21 @@ def number_of_subscribers(subreddit):
 
     headers = {"User-Agent": "Chrome/88.0.4324.150"}
 
+    BASE_URL = "https://www.reddit.com/r"
+
     url = "{}/{}/about.json".format(BASE_URL, subreddit)
 
     try:
         res = requests.get(url, headers=headers, allow_redirects=False)
+        if res.status_code != 200:
+            return 0
+
         results = res.json()
         data = results.get("data")
         subscribers = data.get("subscribers")
+
+        if subscribers is None:
+            return 0
 
         return subscribers
     except Exception:
